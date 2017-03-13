1dd2b0a40451c7e49941ac55bcf470f61b7599ec
Antonio <antonio.hdez93@gmail.com>
2017-03-12 23:11:57 -0600

    Components created, basic layout, and router.

--- js/app.js
+++ js/app.js
@@ -6,27 +6,132 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var BrowserHistory = ReactRouter.browserHistory;
 
-var helpers = require("./helpers");
+var helpers = require("./helpers").default;
+
+var Header = React.createClass({
+    mixins: [BrowserHistory],
+    goTo: function(e) {
+        this.push(e.target.dataset.href);
+    },
+    render: function() {
+        return <nav className="navbar navbar-info navbar-fixed-top">
+                    <div className="container">
+                        <div className="navbar-header">
+                            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#example-navbar-info"><span className="sr-only">Toggle navigation</span><span className="icon-bar"></span><span className="icon-bar"></span><span className="icon-bar"></span></button>
+                            <a className="navbar-brand" data-href="/" onClick={this.goTo} >URL Shortener</a>
+                        </div>
+                        <div className="collapse navbar-collapse" id="example-navbar-info">
+                            <ul className="nav navbar-nav navbar-right">
+                                <li className="active">
+                                    <a data-href="/create/" onClick={this.goTo}>
+                                        Create short url!
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>;
+    }
+})
+
+var Details = React.createClass({
+    getInitialState: function() {
+        console.log(this.props);
+        return {
+            url: this.props.data.url,
+            shortUrl: this.props.data.shortUrl,
+            created: this.props.data.created
+        };
+    },
+    render: function() {
+        return <div className="sh-list-item">
+                    <div className="sh-list-item-main-column">{this.state.url}</div>
+                    <div className="sh-list-item-secondary-column">{this.state.shortUrl}</div>
+                    <div className="sh-list-item-secondary-column">{this.state.created}</div>
+                </div>;
+    }
+});
+
+var List = React.createClass({
+    getInitialState: function() {
+        return {
+            urls: [
+                {
+                    url: "twitter.com/toonyhc",
+                    shortUrl: "/tohc",
+                    created: "10/Feb/2017"
+                },
+                {
+                    url: "facebook.com/me",
+                    shortUrl: "/fbme",
+                    created: "12/May/2016"
+                },
+                {
+                    url: "google.com.mx",
+                    shortUrl: "/goog",
+                    created: "29/Dec/2016"
+                }
+            ]
+        }
+    },
+    render: function() {
+        return <div>
+            <div>
+                <h3>List of URLs</h3>
+            </div>
+            <div>
+                <div className="sh-list-item sh-list-item-headers">
+                    <div className="sh-list-item-main-column">Original Url</div>
+                    <div className="sh-list-item-secondary-column">Short Url</div>
+                    <div className="sh-list-item-secondary-column">Created</div>
+                </div>
+                {this.state.urls.map(function(url){
+                    return <Details data={url} key={helpers.generate_UUID()} />
+                })}
+            </div>
+        </div>;
+    }
+});
 
 var App = React.createClass({
     render: function() {
         return (
             <div>
-                App
+                <Header />
+                <div className="container sh-container">
+                    <List />
+                </div>
             </div>
         )
     }
 });
 
+var CreateUrl = React.createClass({
+    render: function() {
+        return <div>
+                    <Header />
+                    <div className="container sh-container">
+                        <h1>Creating new url</h1>
+                    </div>
+                </div>;
+    }
+});
+
 var NotFound = React.createClass({
     render: function() {
-        return <h1>Not Found :(</h1>;
+        return <div>
+                    <Header />
+                    <div className="container sh-container">
+                        <h1>Not Found :(</h1>
+                    </div>
+                </div>;
     }
 });
 
 var routes = (
     <Router history={BrowserHistory}>
         <Route path="/" components={App} />
+        <Route path="/create" components={CreateUrl} />
         <Route path="*" components={NotFound} />
     </Router>
 );
--- js/helpers.js
+++ js/helpers.js
@@ -1,5 +1,10 @@
 let helpers =  {
-    
+	generate_UUID: function() { //RFC4122 version 4
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+		    return v.toString(16);
+		});
+	}    
 }
 
 export default helpers;
